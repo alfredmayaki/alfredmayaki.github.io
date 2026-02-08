14dd475bfec2618c81470d448ba007a7405f92e5
Alfred Anate Bodurin MAYAKI <alfred.mayaki@ibrecruitment.com>
2026-02-08 19:09:10 +0000

    Disrete omit of 'Thinking...' bubble

--- index.js
+++ index.js
@@ -658,7 +658,7 @@
     state.isSending = true;
     setInputEnabled(false);
 
-    const bubble = addBotBubble('🧑🏿‍💻💭 Thinking...');
+    const bubble = addBotBubble('Thinking...');
 
     const controller = new AbortController();
     state.inFlightAbort = controller;
